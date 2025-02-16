4acf3d178f987614e750ce3abff6562359b1790d
Cauã Campos <campos.eet@gmail.com>
2025-02-16 01:00:20 -0300

    Header and Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import "./global.css"
+import styles from "./App.module.css";
+
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./Post";
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Cauã Campos"
+            content="Teakfnmjawio nam wagfjkolawngawipklg a,.g awgioawgljkawgiawjgfa gawmgnawgikolpawmngawk,g aw."
+          />
+          <Post 
+            author="Cauã Ambrozio"
+            content="Teakfnmjawio nam wagfjkolawngawipklg a,.g awgioawgljkawgiawjgfa gawmgnawgikolpawmngawk,g aw."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
