d2ec4fc7e262aaa14af06a63821091fbf1d2412c
Cauã Campos <campos.eet@gmail.com>
2025-02-19 18:34:34 -0300

    Finished

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,8 +2,21 @@ import styles from "./Comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
 
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/Iagoliveira05.png"/>
@@ -16,7 +29,7 @@ export function Comment({ content }) {
                             <time title="16 de Fevereiro às 23:42" dateTime="2025-02-16">Cerca de 2h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={20}/>
                         </button>
                     </header>
@@ -25,9 +38,9 @@ export function Comment({ content }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
